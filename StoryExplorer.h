6ac17871b60d266a26bca0d5fd8940293f585074
Leonhard Seidel <leonhardseidel@gmx.de>
2022-06-02 19:58:08 +0200

    Add project files.

---
+++ StoryExplorer.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifndef STORYEXPLORER_H
+#define STORYEXPLORER_H
+
+
+#include "targetver.h"
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files
+#include <windows.h>
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+#include "resource.h"
+
+
+#endif // !STORYEXPLORER_H
